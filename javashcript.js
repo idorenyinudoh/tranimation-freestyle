4f03f827086eda5206a790b16de0431cdb24e087
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-05-17 15:14:59 +0100

    Update javashcript.js
    Add event listeners to rotate icon with functions

--- javashcript.js
+++ javashcript.js
@@ -1,6 +1,7 @@
 var show = document.getElementById('show');
 var box = document.getElementById('random-box');
 var button = document.getElementById('button');
+var icon = document.getElementById('icon');
 
 var toggleBox = () => {
     if(box.className == 'show'){
@@ -9,6 +10,17 @@ var toggleBox = () => {
         box.className = 'show';
     }
 }
+var toggleTurn = () => {
+    if(icon.classList.contains('turn')){
+        icon.classList.add('unturn');
+        icon.classList.remove('turn');
+    } else{
+        icon.classList.add('turn');
+        icon.classList.remove('unturn');
+    }
+}
 
 button.addEventListener('focus', toggleBox);
+button.addEventListener('focus', toggleTurn);
 button.addEventListener('blur', toggleBox);
+button.addEventListener('blur', toggleTurn);
