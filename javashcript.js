f1625a80d61bdf1c39bdfdf369c9c55ba1e6df14
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-05-21 14:25:10 +0100

    Move pause upwards on click
    And change move to movedown

--- javashcript.js
+++ javashcript.js
@@ -3,5 +3,9 @@ const pause = document.getElementById('pause');
 
 play.addEventListener('click', () => {
     play.classList.add('turn');
-    pause.classList.add('move');
-})
+    pause.classList.add('movedown');
+});
+pause.addEventListener('click', () => {
+    pause.classList.add('moveup');
+    pause.classList.remove('move');
+});
